8c828a831779f39e395554b890b246e5abdc9ee5
Jacky.L <liujiaqiid@gmail.com>
2018-01-10 09:24:46 +0800

    Local doc chg;

--- manage/admin.py
+++ manage/admin.py
@@ -1,6 +1,7 @@
+"""manage.admin
+"""
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-from django.contrib import admin
+# from django.contrib import admin
 
 # Register your models here.
--- manage/apps.py
+++ manage/apps.py
@@ -1,8 +1,11 @@
+"""manage.apps
+"""
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-
 class ManageConfig(AppConfig):
+    """manage config
+    """
     name = 'manage'
